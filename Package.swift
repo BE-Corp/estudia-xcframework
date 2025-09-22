83d973e4642daa7be7349bd7c90c37a486a85ea5
bauermateus <bauer.mmateus@gmail.com>
2025-09-22 16:00:42 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ComposeApp",
-            url: "https://github.com/BE-Corp/estudia-xcframework/releases/download/1.4/ComposeApp.xcframework.zip", // ajuste para o seu release/tag/arquivo
-            checksum: "a6fece0148b655cd1fbbd76654a86ac5ff983d54e6bb815d26464d908adfae52" // checksum SHA256 gerado para o zip
+            url: "https://github.com/BE-Corp/estudia-xcframework/releases/download/1.4/ComposeApp.xcframework.zip",
+            checksum: "a6fece0148b655cd1fbbd76654a86ac5ff983d54e6bb815d26464d908adfae52"
         ),
     ]
 )
